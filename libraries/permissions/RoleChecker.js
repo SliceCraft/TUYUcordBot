8ea34edbd5637115a3c99c353d18212812eb31aa
SliceCraft <51338223+SliceCraft@users.noreply.github.com>
2025-05-18 02:45:46 +0200

    Add a role checker

---
+++ libraries/permissions/RoleChecker.js
@@ -0,0 +1,19 @@
+class RoleChecker{
+    static isTrialOrAbove(member){
+        if(this.isModOrAbove(member)) return true;
+
+        return member.roles.cache.has(process.env.TRIAL_ROLE);
+    }
+
+    static isModOrAbove(member){
+        if(this.isAdmin(member)) return true;
+
+        return member.roles.cache.has(process.env.MOD_ROLE_ROLES);
+    }
+
+    static isAdmin(member){
+        return member.roles.cache.has(process.env.ADMIN_ROLE) || member.roles.cache.has(process.env.SERVER_TECHNICIAN_ROLE);
+    }
+}
+
+export default RoleChecker;
