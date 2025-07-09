d87d1dbde4a2f14f988fa899eb5b35b7eec2c1fb
SliceCraft <51338223+SliceCraft@users.noreply.github.com>
2025-07-09 18:18:21 +0200

    Implement strike command

---
+++ modules/commands/moderation/strike.js
@@ -0,0 +1,97 @@
+import StrikeUser from "../../../libraries/strikes/StrikeUser.js";
+import {EmbedBuilder} from "discord.js";
+import StrikeChannelAnnouncementManager from "../../../libraries/strikes/StrikeChannelAnnouncementManager.js";
+
+/**
+ * Create an embed for the member about the strike they received
+ * @param strike The strike that was just given to them
+ * @param strikeUser The strike user instance containing info about the user strikes
+ * @returns EmbedBuilder The constructed embed
+ */
+function createStrikeAwardEmbed(strike, strikeUser){
+    let strikeAwardedEmbed = new EmbedBuilder();
+    strikeAwardedEmbed.setColor(0xFF0000);
+    strikeAwardedEmbed.setTitle('You got a strike!');
+    strikeAwardedEmbed.setDescription(`
+        A moderator gave you a strike, you now have ${strikeUser.getActiveStrikeCount()} active strikes.
+        If you reach 3 strikes you will get banned from the server.
+        
+        The reason provided by the moderator is as follows:
+        ${strike.reason}
+    `);
+
+    return strikeAwardedEmbed;
+}
+
+/**
+ * Send a dm to the member about the strike they received
+ * @param user The user getting the strike
+ * @param strike The strike that was just given to them
+ * @param strikeUser The strike user instance containing info about the user strikes
+ * @returns {Promise<boolean>} Whether sending the dm succeeded
+ */
+async function sendDmToMember(user, strike, strikeUser) {
+    try {
+        await user.send({
+            embeds: [createStrikeAwardEmbed(strike, strikeUser)],
+        })
+    } catch {
+        return false;
+    }
+    return true;
+}
+
+/**
+ * Create a temporary channel for a member about the strike they received
+ * @param user The user getting the strike
+ * @param strike The strike that was just given to them
+ * @param strikeUser The strike user instance containing info about the user strikes
+ */
+async function createTempStrikeChannel(user, strike, strikeUser){
+    let channel = await StrikeChannelAnnouncementManager.createChannel(user);
+    StrikeChannelAnnouncementManager.addChannel(channel.id, user.id);
+
+    channel.send({
+        content: `<@${user.id}>`,
+        embeds: [createStrikeAwardEmbed(strike, strikeUser)],
+    });
+}
+
+export default {
+    "name": "strike",
+    "category": "general",
+    "description": "Give a member a strike",
+    "options": [
+        {
+            name: "user",
+            description: "The user you want to give a strike",
+            type: "USER",
+            required: true
+        },
+        {
+            name: 'reason',
+            description: 'The reason for giving a strike',
+            type: 'STRING',
+            required: true
+        }
+    ],
+    async execute(interaction){
+        await interaction.deferReply({ephemeral: true});
+
+        let user = interaction.options.getUser('user');
+        let reason = interaction.options.getString('reason');
+
+        let strikeUser = StrikeUser.getUser(user.id);
+
+        let strike = strikeUser.addStrike(reason, interaction.user.id);
+        strikeUser.save();
+
+        let dmSucceeded = await sendDmToMember(user, strike, strikeUser);
+
+        if (!dmSucceeded) {
+            await createTempStrikeChannel(user, strike, strikeUser);
+        }
+
+        await interaction.editReply({content: 'Strike added successfully'});
+    }
+};
