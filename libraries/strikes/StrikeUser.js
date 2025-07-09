0fea8f959b91ea43d0bb3710af86aae948b71adf
SliceCraft <51338223+SliceCraft@users.noreply.github.com>
2025-07-09 18:16:52 +0200

    Add strike counts and strike expiry

--- libraries/strikes/StrikeUser.js
+++ libraries/strikes/StrikeUser.js
@@ -1,6 +1,11 @@
 import fs from "fs";
 import Strike from "./Strike.js";
 
+/**
+ * @type {number} The delta between each strike expire
+ */
+const STRIKE_EXPIRE_DELTA = 90 * 24 * 60 * 60 * 1000;
+
 class StrikeUser {
     #strikes;
 
@@ -39,6 +44,46 @@ class StrikeUser {
         fs.writeFileSync(`./data/strikes/${this.userid}.json`, JSON.stringify({
             strikes: this.#strikes
         }));
+    }
+
+    getActiveStrikes(){
+        let sortedStrikes = this.#strikes.sort((a,b) => a.createdAt - b.createdAt);
+
+        let strikesForgiven = 0;
+
+        for(let i = 0; i < sortedStrikes.length; i++){
+            let strike = sortedStrikes[i];
+            let nextStrike = sortedStrikes[i + 1];
+
+            let currentStrikeTimestamp = strike.createdAt;
+            let nextStrikeTimestamp = nextStrike ? nextStrike.createdAt : Date.now();
+
+            let newStrikesForgiven = strikesForgiven + Math.floor((nextStrikeTimestamp - currentStrikeTimestamp) / STRIKE_EXPIRE_DELTA)
+
+            // You can't forgive more strikes then you already have
+            // To prevent this from happening we only allow i + 1 (the current amount of strikes) to be forgiven
+            strikesForgiven = Math.min(newStrikesForgiven, i + 1);
+        }
+
+        for(let i = 0; i < sortedStrikes.length; i++){
+            let strike = sortedStrikes[i];
+
+            if(i < strikesForgiven) {
+                strike.active = false;
+            }else{
+                strike.active = true;
+            }
+        }
+
+        return sortedStrikes;
+    }
+
+    getStrikeCount(){
+        return this.#strikes.length;
+    }
+
+    getActiveStrikeCount(){
+        return this.getActiveStrikes().filter(strike => strike.active).length;
     }
 }
 
